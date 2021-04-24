27ada71c05f9106e957cccec17d9910664beeaf6
Sipfindel <50636816+Sipfindel@users.noreply.github.com>
2021-04-24 16:21:59 +0200

    sound system overhaul

--- Autoload/MusicSystem/AnimationPlayer.gd
+++ Autoload/MusicSystem/AnimationPlayer.gd
@@ -1,17 +1,37 @@
 extends AnimationPlayer
 
+onready var hihats = get_node("../AudioStreams/HH").get_children()
+onready var kicks = get_node("../AudioStreams/KD").get_children()
+onready var snares = get_node("../AudioStreams/SN").get_children()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+
+
 signal onBeat
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 func onBeat():
 	emit_signal("onBeat")
+	
+
+	
+func update_tracks():
+	for hihat in hihats:
+		hihat.volume_db = -60.0
+	for kick in kicks:
+		kick.volume_db = -60.0
+	for snare in snares:
+		snare.volume_db = -60.0
+	
+	hihats[rand_range(0, hihats.size())].volume_db = 0.0
+	kicks[rand_range(0, kicks.size())].volume_db = 0.0
+	snares[rand_range(0, snares.size())].volume_db = 0.0
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
