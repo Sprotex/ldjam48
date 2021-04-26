fc08402d5740144fed9d8f03be671eacda2f9454
Sprotex <57769668+Sprotex@users.noreply.github.com>
2021-04-26 21:52:04 +0200

    add preparation api for animation playing

---
+++ Actor Scenes/Enemy/Inherited/Animation Control.gd
@@ -0,0 +1,7 @@
+extends Spatial
+
+func play_idle():
+	pass
+
+func play_attack():
+	pass
---
+++ Actor Scenes/Player/Inherited/Animation Control.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+onready var animator = get_node("AnimationPlayer")
+
+func play_planting():
+	pass
+
+func play_idle():
+	pass
